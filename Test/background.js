828d9d6974d7365b40759136ba2fac9d0afbe691
ZekiKam <155576230+ZekiKam@users.noreply.github.com>
2024-08-03 06:35:47 +0100

    version 3

--- Test/background.js
+++ Test/background.js
@@ -5,20 +5,35 @@ chrome.runtime.onInstalled.addListener(() => {
 const shortsUrl = 'https://www.youtube.com/shorts';
 const reelsUrl = 'https://www.instagram.com/reels';
 
-
 chrome.action.onClicked.addListener((tab) => {
+    console.log("button clicked");
     if (tab.url.startsWith(shortsUrl) || tab.url.startsWith(reelsUrl)) {
         chrome.action.getBadgeText({ tabId: tab.id }, (prevState) => {
             const nextState = prevState === "ON" ? "OFF" : "ON";
 
             chrome.action.setBadgeText({ tabId: tab.id, text: nextState });
 
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                files: ['contentScript.js']
-            });
+            // Inject script only if state is "ON" and script is not already injected
+            if (nextState === "ON") {
+                chrome.scripting.executeScript({
+                    target: { tabId: tab.id },
+                    files: ['contentScript.js']
+                });
+            }
 
+            // Send message to content script to enable/disable interruptions
             chrome.tabs.sendMessage(tab.id, { action: nextState === "ON" ? "enableInterrupt" : "disableInterrupt" });
+        });
+    } else {
+        // Handle case where URL is not valid
+        chrome.action.setBadgeText({ tabId: tab.id, text: "OFF" });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: showAlert
         });
     }
 });
+
+function showAlert() {
+    alert("This extension only works for YouTube Shorts and Instagram Reels.");
+}
--- Test/popup.js
+++ Test/popup.js
@@ -1,12 +1,15 @@
 document.getElementById("countdown").addEventListener('click',() =>{
-    alert("Count down is selected");
+    chrome.runtime.sendMessage({action: "setOption", option:"countdown"});
 });
 
 document.getElementById('puzzle').addEventListener('click', () => {
-    alert('Puzzle selected');
+    chrome.runtime.sendMessage({action: "setOption", option:"puzzle"});
 });
   
 document.getElementById('quote').addEventListener('click', () => {
-    alert('Quote selected');
+    chrome.runtime.sendMessage({action: "setOption", option:"quote"});
 });
   
+//Send message (option) to background.js and it will direct the message to contentScript.js
+//Can't send message to contentScript.js directly because they run in separate execution environments (popup.js: popup, contextScript.js: webpage)
+//To facilitate communication between them, we need a service worker as a mediator, which is background.js
